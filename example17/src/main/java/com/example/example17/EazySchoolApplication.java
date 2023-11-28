328865f4fa4fdc927945d27e1d59188b52edc4a3
jungminyoo <yoojm4718@outlook.com>
2023-11-29 02:41:31 +0900

    Example 17: Added Home Controller

--- example17/src/main/java/com/example/example17/EazySchoolApplication.java
+++ example17/src/main/java/com/example/example17/EazySchoolApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Example17Application {
+public class EazySchoolApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Example17Application.class, args);
+		SpringApplication.run(EazySchoolApplication.class, args);
 	}
 
 }
--- example17/src/test/java/com/example/example17/EazySchoolApplicationTests.java
+++ example17/src/test/java/com/example/example17/EazySchoolApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Example17ApplicationTests {
+class EazySchoolApplicationTests {
 
 	@Test
 	void contextLoads() {
